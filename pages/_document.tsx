acdd6c4e78db2d6de477818d4b391a073a8e8ccf
Mateus Tavares <62652109+Mateus8741@users.noreply.github.com>
2023-08-05 03:27:56 -0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="shortcut icon" href="/static/logos/icon_logo.svg" />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
